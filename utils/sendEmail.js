dc2fa42bcbd3af7ba159cdaa2eb3a858b4c6aa2a
Muhammad Kashan <112327092+Kashan724@users.noreply.github.com>
2024-07-04 15:48:45 +0500

    updated code changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,8 +1,15 @@
-import { TransactionalClient } from '@mailchimp/mailchimp_transactional';
+// Import the entire package and destructure TransactionalClient
+import pkg from '@mailchimp/mailchimp_transactional';
+const { TransactionalClient } = pkg;
 
+// Import dotenv and configure it if not already done globally
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Create an instance of TransactionalClient using your API key
 const mailchimp = new TransactionalClient(process.env.MAILCHIMP_API_KEY);
 
+// Define the sendEmail function to send an email
 export const sendEmail = async (subject, message, send_to, sent_from) => {
   const msg = {
     from_email: sent_from,
@@ -12,6 +19,7 @@ export const sendEmail = async (subject, message, send_to, sent_from) => {
   };
 
   try {
+    // Use mailchimp instance to send email
     const result = await mailchimp.messages.send({ message: msg });
     console.log('Email sent successfully:', result);
     return result;
